0504ee8c1034c257796408ecc664d4baece16da2
Mathias Rüdiger <mathias.ruediger@fromatob.com>
2016-06-06 13:01:19 +0200

    update tests and privatized some methods

--- lib/a2b_logging/formatter.rb
+++ lib/a2b_logging/formatter.rb
@@ -4,12 +4,32 @@ require 'logstash-event'
 module A2bLogging
   class Formatter < ::Logger::Formatter
 
+    def call(severity, time, progname, msg)
+      msg = { 'message' => msg.is_a?(String) ? msg : msg.inspect } unless msg.is_a?(Hash)
+
+      msg['severity'] = severity if severity
+      msg['progname'] = progname if progname
+
+      tags = current_tags
+
+      if tags.size > 0
+        msg['type'] ||= tags.first
+        msg['tags'] = tags
+      end
+
+      event = LogStash::Event.new(msg)
+
+      "%s\n" % event.to_json
+    end
+
     def tagged(*tags)
       new_tags = push_tags(*tags)
       yield self
     ensure
       pop_tags(new_tags.size)
     end
+
+  private
 
     def push_tags(*tags)
       tags.flatten.reject(&:blank?).tap do |new_tags|
@@ -27,24 +47,6 @@ module A2bLogging
 
     def current_tags
       Thread.current[:activesupport_tagged_logging_tags] ||= []
-    end
-
-    def call(severity, time, progname, msg)
-      msg = { 'message' => msg.is_a?(String) ? msg : msg.inspect } unless msg.is_a?(Hash)
-
-      msg['severity'] = severity if severity
-      msg['progname'] = progname if progname
-
-      tags = current_tags
-
-      if tags.size > 0
-        msg['type'] ||= tags.first
-        msg['tags'] = tags
-      end
-
-      event = LogStash::Event.new(msg)
-
-      "%s\n" % event.to_json
     end
   end
 end
--- spec/a2b_formatter_spec.rb
+++ spec/a2b_formatter_spec.rb
@@ -2,49 +2,76 @@ require 'spec_helper'
 require 'active_support'
 
 RSpec.describe A2bLogging::Formatter, "A2BFormatter" do
-  context "formatting a string" do
-    strio = StringIO.new
+  describe "the json output" do
+    subject { JSON.parse(strio.string) }
 
-    # match rails logger setup
-    logger = ActiveSupport::Logger.new strio
-    logger = ActiveSupport::TaggedLogging.new(logger)
-    logger.formatter = A2bLogging::Formatter.new
+    let(:strio) { StringIO.new }
+    let(:logger) { ActiveSupport::Logger.new(strio) }
+    let(:tagged_logger) { ActiveSupport::TaggedLogging.new(logger) }
 
-    logger.info "hello world"
-    json = JSON.parse(strio.string)
+    context "when not using tags" do
+      before do
+        tagged_logger.formatter = A2bLogging::Formatter.new
+        tagged_logger.info log_input
+      end
 
-    it "uses the string as message" do
-      expect(json['message']).to eq "hello world"
+      context "and logging a string" do
+        let(:log_input) { "hello world" }
+
+        it "uses the string as message" do
+          expect(subject['message']).to eq "hello world"
+        end
+
+        it "has a correct timestamp" do
+          expect(Time.parse(subject['@timestamp'])).to be_within(2).of Time.now
+        end
+      end
+
+      context "and logging an object" do
+        let(:log_input) { { type: "foo", test: 123 } }
+
+        it "contains the data in the correct field" do
+          expect(subject['test']).to eq 123
+        end
+
+        it "has a correct timestamp" do
+          expect(Time.parse(subject['@timestamp'])).to be_within(2).of Time.now
+        end
+
+        it "has a field type containing the type" do
+          expect(subject['type']).to eq "foo"
+        end
+      end
     end
 
-    it "has a correct timestamp" do
-      expect(Time.parse(json['@timestamp'])).to be_within(2).of Time.now
+    context "when using tags" do
+      before do
+        tagged_logger.formatter = A2bLogging::Formatter.new
+        tagged_logger.tagged("test_type") do
+          tagged_logger.tagged("test_tag") do
+            tagged_logger.info "foobar"
+          end
+        end
+      end
+
+      it "uses the string as message" do
+        expect(subject['message']).to eq "foobar"
+      end
+
+      it "has a correct timestamp" do
+        expect(Time.parse(subject['@timestamp'])).to be_within(2).of Time.now
+      end
+
+      it "has the correct type" do
+        expect(subject['type']).to eq "test_type"
+      end
+
+      it "has the correct tags" do
+        expect(subject['tags']).to eq ["test_type","test_tag"]
+      end
     end
   end
 
-  context "formatting an object" do
-    strio = StringIO.new
-
-    # match rails logger setup
-    logger = ActiveSupport::Logger.new strio
-    logger = ActiveSupport::TaggedLogging.new(logger)
-    logger.formatter = A2bLogging::Formatter.new
-
-    logger.info( { "type" => "foo", "test" => 123 } )
-    json = JSON.parse(strio.string)
-
-    it "contains the data in the correct field" do
-      expect(json['test']).to eq 123
-    end
-
-    it "has a correct timestamp" do
-      expect(Time.parse(json['@timestamp'])).to be_within(2).of Time.now
-    end
-
-    it "has a field type containing the type" do
-      expect(json['type']).to eq "foo"
-    end
-  end
 
   context "using tagged logging" do
     strio = StringIO.new
