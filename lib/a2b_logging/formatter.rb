8ebbe98b3b269e81ab6d886b6e3bd5c8ffad13b3
Mathias Rüdiger <mathias.ruediger@fromatob.com>
2016-06-06 15:57:45 +0200

    removed c&p methods and imported them instead

--- lib/a2b_logging/formatter.rb
+++ lib/a2b_logging/formatter.rb
@@ -1,8 +1,11 @@
 require 'logger'
 require 'logstash-event'
+require 'active_support'
 
 module A2bLogging
   class Formatter < ::Logger::Formatter
+
+    include ActiveSupport::TaggedLogging::Formatter
 
     def call(severity, time, progname, msg)
       msg = { 'message' => msg.is_a?(String) ? msg : msg.inspect } unless msg.is_a?(Hash)
@@ -20,33 +23,6 @@ module A2bLogging
       event = LogStash::Event.new(msg)
 
       "%s\n" % event.to_json
-    end
-
-    def tagged(*tags)
-      new_tags = push_tags(*tags)
-      yield self
-    ensure
-      pop_tags(new_tags.size)
-    end
-
-  private
-
-    def push_tags(*tags)
-      tags.flatten.reject(&:blank?).tap do |new_tags|
-        current_tags.concat new_tags
-      end
-    end
-
-    def pop_tags(size = 1)
-      current_tags.pop size
-    end
-
-    def clear_tags!
-      current_tags.clear
-    end
-
-    def current_tags
-      Thread.current[:activesupport_tagged_logging_tags] ||= []
     end
   end
 end
