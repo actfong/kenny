29eea7ee88a9a56910a95624e6cf36913efb6888
Alex Fong <actfong@gmail.com>
2016-07-01 16:11:36 +0200

    Removed redundant references to Version

--- lib/a2b_logging.rb
+++ lib/a2b_logging.rb
@@ -1,6 +1,1 @@
-require "a2b_logging/version"
 require "a2b_logging/formatter"
-
-module A2bLogging
-
-end
--- spec/a2b_logging_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe A2bLogging do
-  it 'has a version number' do
-    expect(A2bLogging::VERSION).not_to be nil
-  end
-end
