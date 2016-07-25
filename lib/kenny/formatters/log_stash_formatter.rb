263559dff85edcd15963671ebd35bb8f3195bedc
Alex Fong <actfong@gmail.com>
2016-07-25 09:25:57 +0200

    Changed LogStashFormatter to take type from progname, which has the highest precedence.

--- lib/kenny/formatters/log_stash_formatter.rb
+++ lib/kenny/formatters/log_stash_formatter.rb
@@ -4,22 +4,19 @@ require 'active_support'
 
 module Kenny
   module Formatters
+    ##
     # Formats messages as LogStash::Event
+    # the 'type' field can be used for ElasticSearch
+    # The 'type' could be set through the Logger's progname,
+    # which takes the highest precedence.
+    # If logger.progname.nil?, it will take the 'type' within the hash.
+    # If the 'type' within the Hash is also nil,
+    # then you can set the type through FileBeat's config
     class LogStashFormatter < ::Logger::Formatter
-      include ActiveSupport::TaggedLogging::Formatter
-
       def call(severity, time, progname, msg)
         msg = { 'message' => msg.is_a?(String) ? msg : msg.inspect } unless msg.is_a?(Hash)
-
         msg['severity'] = severity if severity
-        msg['progname'] = progname if progname
-
-        tags = current_tags
-
-        if tags.any?
-          msg['type'] ||= tags.first
-          msg['tags'] = tags
-        end
+        msg['type'] = progname if progname
 
         event = LogStash::Event.new(msg)
 
--- spec/formatters/logstash_formatter_spec.rb
+++ spec/formatters/logstash_formatter_spec.rb
@@ -7,12 +7,11 @@ RSpec.describe Kenny::Formatters::LogStashFormatter do
 
     let(:strio) { StringIO.new }
     let(:logger) { ActiveSupport::Logger.new(strio) }
-    let(:tagged_logger) { ActiveSupport::TaggedLogging.new(logger) }
 
-    context 'when not using tags' do
+    context 'progname not set in logger' do
       before do
-        tagged_logger.formatter = Kenny::Formatters::LogStashFormatter.new
-        tagged_logger.info log_input
+        logger.formatter = Kenny::Formatters::LogStashFormatter.new
+        logger.info log_input
       end
 
       context 'and logging a string' do
@@ -25,50 +24,63 @@ RSpec.describe Kenny::Formatters::LogStashFormatter do
         it 'has a correct timestamp' do
           expect(Time.parse(subject['@timestamp'])).to be_within(2).of Time.now
         end
+
+        it { expect(subject['type']).to be_nil }
       end
 
       context 'and logging an object' do
-        let(:log_input) { { type: 'foo', test: 123 } }
+        context 'type set in hash' do
+          let(:log_input) { { type: 'foo', test: 123 } }
 
-        it 'contains the data in the correct field' do
-          expect(subject['test']).to eq 123
-        end
+          it 'contains the data in the correct field' do
+            expect(subject['test']).to eq 123
+          end
 
-        it 'has a correct timestamp' do
-          expect(Time.parse(subject['@timestamp'])).to be_within(2).of Time.now
-        end
+          it 'has a correct timestamp' do
+            expect(Time.parse(subject['@timestamp'])).to be_within(2).of Time.now
+          end
 
-        it 'has a field type containing the type' do
-          expect(subject['type']).to eq 'foo'
-        end
-      end
-    end
-
-    context 'when using tags' do
-      before do
-        tagged_logger.formatter = Kenny::Formatters::LogStashFormatter.new
-        tagged_logger.tagged('test_type') do
-          tagged_logger.tagged('test_tag') do
-            tagged_logger.info 'foobar'
+          it 'has a field type containing the type' do
+            expect(subject['type']).to eq 'foo'
           end
         end
-      end
 
-      it 'uses the string as message' do
-        expect(subject['message']).to eq 'foobar'
-      end
+        context 'type NOT set in hash' do
+          let(:log_input) { { test: 123 } }
 
-      it 'has a correct timestamp' do
-        expect(Time.parse(subject['@timestamp'])).to be_within(2).of Time.now
-      end
-
-      it 'has the correct type' do
-        expect(subject['type']).to eq 'test_type'
-      end
-
-      it 'has the correct tags' do
-        expect(subject['tags']).to eq %w(test_type test_tag)
+          it { expect(subject['type']).to be_nil }
+        end
       end
     end
+
+    context 'progname has been set in logger' do
+      before do
+        logger.formatter = Kenny::Formatters::LogStashFormatter.new
+        logger.progname = 'aspector'
+        logger.info log_input
+      end
+
+      context 'logging a string' do
+        let(:log_input) { 'hello world' }
+        it 'uses the progname as type' do
+          expect(subject['type']).to eq 'aspector'
+        end
+      end
+
+      context 'logging a Hash without type in it' do
+        let(:log_input) { { test: 123 } }
+        it 'uses the logger.progname as type' do
+          expect(subject['type']).to eq 'aspector'
+        end
+      end
+
+      context 'logging a Hash with type in it' do
+        let(:log_input) { { type: 'foo', test: 123 } }
+        it 'gives progname precedence over type within the hash' do
+          expect(subject['type']).to eq 'aspector'
+        end
+      end
+    end
+
   end
 end
